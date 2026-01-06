3e172749a41f0d1df1d3d85e3a0186bf5ed7fbe4
DESKTOP-5N7PE9V\admin <crazypad@qq.com>
2026-01-06 11:35:16 +0800

    项目配置补充

--- Egg/Assets/Game/Scripts/Player/PlayerCtrl.cs
+++ Egg/Assets/Game/Scripts/Player/PlayerCtrl.cs
@@ -11,7 +11,7 @@ public class PlayerCtrl : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
